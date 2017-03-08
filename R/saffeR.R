9d37bdde571186286417a5e9bb6c1b27f0e0eac9
Jon Calder <jonmcalder@gmail.com>
2017-03-08 16:24:52 +0200

    Add package documentation/help page

---
+++ R/saffeR.R
@@ -0,0 +1,7 @@
+#' saffeR: Things relating to South Africa
+#'
+#' The saffeR package provides a collection of data and functions for things relevant to South Africa.
+#'
+#' @docType package
+#' @name saffeR
+NULL
