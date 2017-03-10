0859f62a04af51764e9a965b13c5225fe7ff0cb3
Andrew Collier <andrew@exegetic.biz>
2017-03-10 16:13:52 +0200

    Handle postal codes that are not numeric in postalcodes_province.

--- R/postalcodes_province.R
+++ R/postalcodes_province.R
@@ -4,9 +4,8 @@
 #'
 #' @export
 postalcodes_province <- function(code) {
+  if (class(code) == "character") code = suppressWarnings(as.integer(code))
   if(is.na(code) || is.null(code)) return(NA)
-
-  if (class(code) == "character") code = as.integer(code)
 
   if ((code >= 1 && code <= 299) || (code >= 1400 && code <= 2199)) {
     return("Gauteng")
