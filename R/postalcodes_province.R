302cc61884b7772362257ec12c20f437f11a7194
Andrew Collier <andrew@exegetic.biz>
2017-03-10 15:58:41 +0200

    postalcodes_province handles NA/NULL input.

--- R/postalcodes_province.R
+++ R/postalcodes_province.R
@@ -2,7 +2,10 @@
 #'
 #' @source \url{https://en.wikipedia.org/wiki/List_of_postal_codes_in_South_Africa}
 #'
+#' @export
 postalcodes_province <- function(code) {
+  if(is.na(code) || is.null(code)) return(NA)
+
   if (class(code) == "character") code = as.integer(code)
 
   if ((code >= 1 && code <= 299) || (code >= 1400 && code <= 2199)) {
