29d38023a759e5c1428d4f2b9cb804a6a0ec0675
Andrew B. Collier <andrew@exegetic.biz>
2017-06-28 04:11:49 +0200

    Wrapped all code in conditional.

--- data-raw/prepare-postal-codes.R
+++ data-raw/prepare-postal-codes.R
@@ -1,16 +1,18 @@
-postalcodes <- read.delim(file.path("data-raw", "postalcodes.csv"), sep = ";", stringsAsFactors = FALSE, comment.char = "#")
+if (FALSE) {
+  postalcodes <- read.delim(file.path("data-raw", "postalcodes.csv"), sep = ";", stringsAsFactors = FALSE, comment.char = "#")
 
-library(dplyr)
+  library(dplyr)
 
-postalcodes <- mutate(postalcodes,
-                      BoxCode = ifelse(is.na(BoxCode), NA, sprintf("%04d", BoxCode)),
-                      StreetCode = ifelse(is.na(StreetCode), NA, sprintf("%04d", StreetCode))
-) %>% mutate(
-  Province = factor(sapply(ifelse(is.na(StreetCode), BoxCode, StreetCode), postalcodes_province))
-) %>% select(-id) %>% select(Province, Area, Suburb, everything())
+  postalcodes <- mutate(postalcodes,
+                        BoxCode = ifelse(is.na(BoxCode), NA, sprintf("%04d", BoxCode)),
+                        StreetCode = ifelse(is.na(StreetCode), NA, sprintf("%04d", StreetCode))
+  ) %>% mutate(
+    Province = factor(sapply(ifelse(is.na(StreetCode), BoxCode, StreetCode), postalcodes_province))
+  ) %>% select(-id) %>% select(Province, Area, Suburb, everything())
 
-names(postalcodes) <- tolower(names(postalcodes))
+  names(postalcodes) <- tolower(names(postalcodes))
 
-library(devtools)
+  library(devtools)
 
-use_data(postalcodes, overwrite = TRUE)
+  use_data(postalcodes, overwrite = TRUE)
+}
--- data-raw/scrape-postal-code-coordinates.R
+++ data-raw/scrape-postal-code-coordinates.R
@@ -1,51 +1,53 @@
-library(rvest)
-library(dplyr)
-library(stringr)
+if (FALSE) {
+  library(rvest)
+  library(dplyr)
+  library(stringr)
 
-URL = "http://www.geonames.org/postalcode-search.html?q=%s&country=ZA"
+  URL = "http://www.geonames.org/postalcode-search.html?q=%s&country=ZA"
 
-codes = levels(postalcodes$streetcode)
+  codes = levels(postalcodes$streetcode)
 
-RSLEEP = 15
+  RSLEEP = 15
 
-scrape.code <- function(code) {
-  while(TRUE) {
-    html <- tryCatch(read_html(sprintf(URL, code)), error = function(e) e)
+  scrape.code <- function(code) {
+    while(TRUE) {
+      html <- tryCatch(read_html(sprintf(URL, code)), error = function(e) e)
+      #
+      if ("error" %in% class(html)) {
+        cat("-> retrying...\n")
+        Sys.sleep(rpois(1, RSLEEP))
+      } else break
+    }
     #
-    if ("error" %in% class(html)) {
-      cat("-> retrying...\n")
-      Sys.sleep(rpois(1, RSLEEP))
-    } else break
+    # Find coordinates convert to numeric matrix.
+    #
+    coordinates <- html %>% html_nodes("td > a > small") %>%
+      html_text() %>%
+      str_split("/") %>%
+      lapply(as.numeric)
+    coordinates <- do.call(rbind, coordinates)
+    #
+    if(is.null(nrow(coordinates))) stop("No data for code ", code, ".")
+    #
+    colnames(coordinates) <- c("lat", "lon")
+    #
+    # There can be multiple entries, so calculate the mean.
+    #
+    colMeans(coordinates) %>% as.list %>% data.frame %>% mutate(code = code) %>% select(code, everything())
   }
+
+  code.coordinates <- lapply(codes, function(code) {
+    cat(code, "\n")
+    tryCatch(scrape.code(code), error = function(e) NULL)
+  })
   #
-  # Find coordinates convert to numeric matrix.
+  # Filter out missing entries.
   #
-  coordinates <- html %>% html_nodes("td > a > small") %>%
-    html_text() %>%
-    str_split("/") %>%
-    lapply(as.numeric)
-  coordinates <- do.call(rbind, coordinates)
+  code.coordinates = Filter(function(x) !is.null(x), code.coordinates)
   #
-  if(is.null(nrow(coordinates))) stop("No data for code ", code, ".")
-  #
-  colnames(coordinates) <- c("lat", "lon")
-  #
-  # There can be multiple entries, so calculate the mean.
-  #
-  colMeans(coordinates) %>% as.list %>% data.frame %>% mutate(code = code) %>% select(code, everything())
+  postalcodes_geo = do.call(rbind, code.coordinates)
+
+  library(devtools)
+
+  use_data(postalcodes_geo, overwrite = TRUE)
 }
-
-code.coordinates <- lapply(codes, function(code) {
-  cat(code, "\n")
-  tryCatch(scrape.code(code), error = function(e) NULL)
-})
-#
-# Filter out missing entries.
-#
-code.coordinates = Filter(function(x) !is.null(x), code.coordinates)
-#
-postalcodes_geo = do.call(rbind, code.coordinates)
-
-library(devtools)
-
-use_data(postalcodes_geo, overwrite = TRUE)
