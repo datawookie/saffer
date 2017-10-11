4cbdb1f7f70cf5d33f9bf7024164b703089afb42
Andrew B. Collier <andrew@exegetic.biz>
2017-10-11 14:37:02 +0200

    Added CPI data.

---
+++ R/cpi.R
@@ -0,0 +1,15 @@
+#' Consumer Price Index for South Africa.
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#'  \item{year}{}
+#'  \item{month}{}
+#'  \item{cpi}{Official inflation rate.}
+#' }
+"cpi"
+
+if (FALSE) {
+  cpi <- read.csv(file.path("data-raw", "consumer-price-index.csv"))
+  save(cpi, file = file.path("data", "cpi.rda"))
+  rm(cpi)
+}
