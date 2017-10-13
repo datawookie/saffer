6d36ec2fbe20997406bb322bb97e3753ddb98208
Andrew B. Collier <andrew@exegetic.biz>
2017-10-13 13:02:21 +0200

    Fixed factor levels for month.

--- R/cpi.R
+++ R/cpi.R
@@ -9,7 +9,10 @@
 "cpi"
 
 if (FALSE) {
-  cpi <- read.csv(file.path("data-raw", "consumer-price-index.csv"))
+  cpi <- read.csv(file.path("data-raw", "consumer-price-index.csv"), stringsAsFactors = FALSE) %>%
+    mutate(
+      month = factor(month, labels = month.abb)
+    )
   save(cpi, file = file.path("data", "cpi.rda"))
   rm(cpi)
 }
