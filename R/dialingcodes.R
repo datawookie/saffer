2ce590d183b43c4332aada8ae0d754f26c363602
vusmas <55582447+vusmas@users.noreply.github.com>
2021-01-13 16:34:39 +0200

    Brief Documentation
     A script for brief documentation of dialing codes

---
+++ R/dialingcodes.R
@@ -0,0 +1,10 @@
+#' Dialing codes for cities in South Africa.
+#'
+#' @format A data frame with 2 columns:
+#' \describe{
+#'  \item{city}{}
+#'  \item{code}{Dialing Code}
+#' }
+#'
+#' @source \url{https://www.rebtel.com/en/international-calling-guide/phone-codes/south-africa/}
+"dialingcodes"
