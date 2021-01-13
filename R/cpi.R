e66d3ec64d5c1859d46e938571d857bfdd1e5e89
Andrew Collier <andrew@exegetic.biz>
2021-01-13 17:40:53 +0200

    Add dialling codes; separate script for CPI

--- R/cpi.R
+++ R/cpi.R
@@ -7,12 +7,3 @@
 #'  \item{cpi}{Official inflation rate.}
 #' }
 "cpi"
-
-if (FALSE) {
-  library(dplyr)
-  cpi <- read.csv(file.path("data-raw", "consumer-price-index.csv"), stringsAsFactors = FALSE) %>%
-    mutate(month = factor(month, levels = month.abb)) %>%
-    arrange(year, month)
-  save(cpi, file = file.path("data", "cpi.rda"))
-  rm(cpi)
-}
