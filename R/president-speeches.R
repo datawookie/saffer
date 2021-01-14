98c9a666c96edacd44f73e609a767c6a533a9f6f
Andrew Collier <andrew@exegetic.biz>
2021-01-14 06:09:23 +0200

    Add president speeches

---
+++ R/president-speeches.R
@@ -0,0 +1,13 @@
+#' Speeches of the President and Deputy President
+#'
+#' @format A data frame with 5 columns:
+#' \describe{
+#'  \item{date}{}
+#'  \item{position}{}
+#'  \item{person}{}
+#'  \item{title}{}
+#'  \item{text}{}
+#' }
+#'
+#' @source \url{http://www.thepresidency.gov.za/speeches/}
+"president_speeches"
