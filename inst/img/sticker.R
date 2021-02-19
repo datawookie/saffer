a13f7a80c47cf708a526f056aff80e0d221ee9a5
Andrew Collier <andrew@exegetic.biz>
2021-02-19 06:22:04 +0200

    Update hex sticker

--- inst/img/sticker.R
+++ inst/img/sticker.R
@@ -1,7 +1,28 @@
 library(hexSticker)
 
-sticker("sa_flag.png", package = "saffer",
-        p_y = 1.6, p_size = 20, p_color = "#FFB612",
-        s_x = 1, s_y = 0.95, s_width = 0.65,
-        h_color = "#FFB612", h_fill = "#007A4D")
+URL_FLAG_SVG = "https://raw.githubusercontent.com/hampusborgos/country-flags/master/svg/za.svg"
+NAME_FLAG_SVG = "za_flag.svg"
+NAME_FLAG_PNG = "za_flag.png"
+
+if (!file.exists(NAME_FLAG_SVG)) {
+  download.file(URL_FLAG_SVG, NAME_FLAG_SVG)
+}
+
+if (!file.exists(NAME_FLAG_PNG)) {
+  system(paste("convert -density 7200", NAME_FLAG_SVG, NAME_FLAG_PNG))
+}
+
+sticker(
+  "za_flag.png",
+  package = "saffer",
+  p_y = 1.6,
+  p_size = 20,
+  p_color = "#000000",
+  p_family = "sans",
+  s_x = 1,
+  s_y = 0.95,
+  s_width = 0.75,
+  h_color = "#000000",
+  h_fill = "#FFFFFF"
+)
 
