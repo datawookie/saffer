baf5945cd86067a49e8fa9c20d865d687a4e53df
Andrew Collier <andrew@exegetic.biz>
2021-02-19 12:26:50 +0200

    Fix documentation warnings

--- R/cpi-average-prices.R
+++ R/cpi-average-prices.R
@@ -3,10 +3,10 @@
 #' @format A data frame with 4 columns:
 #' \describe{
 #'  \item{product}{}
-#'  \item{year}
-#'  \item{month}
-#'  \item{price}
+#'  \item{year}{}
+#'  \item{month}{}
+#'  \item{price}{}
 #' }
 #'
-#' @source \url{http://www.statssa.gov.za/timeseriesdata/Excel/P0141%20-%20CPI%20Average%20Prices%20All%20urban%20(202011).zip }
+#' @source \url{http://www.statssa.gov.za/timeseriesdata/Excel/P0141\%20-\%20CPI\%20Average\%20Prices\%20All\%20urban\%20(202011).zip}
 "cpi_average_prices"
--- R/dialingcodes.R
+++ R/dialingcodes.R
@@ -3,7 +3,7 @@
 #' @format A data frame with 2 columns:
 #' \describe{
 #'  \item{city}{}
-#'  \item{codes}
+#'  \item{codes}{}
 #' }
 #'
 #' @source \url{https://www.rebtel.com/en/international-calling-guide/phone-codes/south-africa/}
