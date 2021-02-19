aa459a6b9bd567dd1cb64dde2ba228b0b3332707
Andrew Collier <andrew@exegetic.biz>
2021-02-19 12:26:50 +0200

    Fix documentation warnings

--- R/dialingcodes.R
+++ R/dialingcodes.R
@@ -3,7 +3,11 @@
 #' @format A data frame with 2 columns:
 #' \describe{
 #'  \item{city}{}
+<<<<<<< HEAD
 #'  \item{code}{Dialing Code}
+=======
+#'  \item{codes}{}
+>>>>>>> 1038b7a... Fix documentation warnings
 #' }
 #'
 #' @source \url{https://www.rebtel.com/en/international-calling-guide/phone-codes/south-africa/}
