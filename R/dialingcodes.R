356f61ea63b9f011a9b4025e0bb618f192259e83
Andrew Collier <andrew@exegetic.biz>
2021-04-06 05:30:07 +0200

    fix: Change column name to 'code'

--- R/dialingcodes.R
+++ R/dialingcodes.R
@@ -3,12 +3,7 @@
 #' @format A data frame with 2 columns:
 #' \describe{
 #'  \item{city}{}
-<<<<<<< HEAD
 #'  \item{code}{Dialing Code}
-=======
-#'  \item{codes}{}
->>>>>>> 1038b7a... Fix documentation warnings
-#' }
 #'
 #' @source \url{https://www.rebtel.com/en/international-calling-guide/phone-codes/south-africa/}
 "dialing_codes"
