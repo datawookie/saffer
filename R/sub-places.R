3746a2b3bc3a31f40ceb3c1284c85f4d97f585ff
Matt Dennis <mattdenni@gmail.com>
2021-06-01 06:12:31 +0200

    feat: Add South African sub-places.

---
+++ R/sub-places.R
@@ -0,0 +1,22 @@
+#' Dataset of Sub-places in South Africa
+#'
+#' A dataset containing the the names of South African sub-places (e.g. suburbs).
+#'
+#' @docType data
+#'
+#' @usage data(sub_places)
+#'
+#' @format A data frame with 21243 records and 8 variables:
+#' \describe{
+#'   \item{sub_place_code}{}
+#'   \item{sub_place}{}
+#'   \item{municipality_code}{}
+#'   \item{municipality}{}
+#'   \item{district_code}{}
+#'   \item{district}{}
+#'   \item{province_code}{}
+#'   \item{province}{}
+#' }
+#'
+#' @source \url{http://www.statssa.gov.za/census/census_2001/SubPlace.zip}
+"sub_places"
