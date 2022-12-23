7d6f5a63593fb19e1b3542b8ed806b892d0e3637
Andrew Collier <andrew@fathomdata.dev>
2022-12-23 10:29:35 +0000

    fix: Download PNG rather than SVG

--- inst/sticker.R
+++ inst/sticker.R
@@ -1,15 +1,10 @@
 library(hexSticker)
 
-URL_FLAG_SVG = "https://raw.githubusercontent.com/hampusborgos/country-flags/master/svg/za.svg"
-NAME_FLAG_SVG = "za_flag.svg"
+URL_FLAG_PNG = "https://github.com/hampusborgos/country-flags/raw/main/png1000px/za.png"
 NAME_FLAG_PNG = "za_flag.png"
 
-if (!file.exists(NAME_FLAG_SVG)) {
-  download.file(URL_FLAG_SVG, NAME_FLAG_SVG)
-}
-
 if (!file.exists(NAME_FLAG_PNG)) {
-  system(paste("convert -density 7200", NAME_FLAG_SVG, NAME_FLAG_PNG))
+  download.file(URL_FLAG_PNG, NAME_FLAG_PNG)
 }
 
 FIGURES_DIR <- here::here("man", "figures")
@@ -17,7 +12,7 @@ FIGURES_DIR <- here::here("man", "figures")
 dir.create(FIGURES_DIR)
 
 sticker(
-  "za_flag.png",
+  NAME_FLAG_PNG,
   package = "{saffer}",
   p_x = 1.215,
   p_y = 1.0225,
@@ -26,7 +21,7 @@ sticker(
   p_family = "mono",
   s_x = 1,
   s_y = 1,
-  s_width = 1.925,
+  s_width = 1.45,
   h_color = "#000000",
   h_fill = "#FFFFFF",
   white_around_sticker = TRUE,
